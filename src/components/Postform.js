52d3d46d2f84537a11ad99b00616208c04ead3d5
Asutosh Payasi <89244793+Ashutoshpayasi@users.noreply.github.com>
2023-03-31 14:22:38 +0530

    Update Postform.js

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -21,7 +21,7 @@ function Postform() {
   useEffect(()=>{
     console.log(url)
     if(url){
-      fetch("http://localhost:8080/addpost",{
+      fetch("https://instabackend-kxq3.onrender.com/addpost",{
         method:"Post",
         headers: {
           "Content-Type": "application/json"
